c33bf8e194a817d80e244ec84b06601234cb56cf
amanchcoudhary9999 <amanighandle@gmail.com>
2025-11-13 03:16:01 +0530

    Use environment variable for SECRET_KEY

--- PingMe/settings.py
+++ PingMe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$%vy8@6-sndqf&a%0@m+=^er8%=q)wxbb9@-35tox@g_#mp63'
+SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,3 +153,6 @@ CSRF_COOKIE_SECURE = False
 
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/rooms/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
